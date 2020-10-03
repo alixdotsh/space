f2d5a4297f709e4e76c0d732f17284dc231130e2
rawr x3 <58949994+rawr-x3@users.noreply.github.com>
2020-10-03 10:01:10 -0400

    Update online.js

--- commands/online.js
+++ commands/online.js
@@ -1,3 +1,3 @@
 module.exports = message => {
-	message.channel.send(`Hi, I am Nurple! My Owners Are **Quarantined Garrison#2320** and **Istérix#1287** aka Mastew Gawwison and Istewix. I Am Ready To Do my Duties :3`)
+	message.channel.send(`Status: Online`)
 }
