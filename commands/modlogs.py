33cba8ac81817f3c3f3e97099f8ca8fe54557615
rawr x3 <58949994+rawr-x3@users.noreply.github.com>
2020-10-04 22:02:23 -0400

    Update modlogs.py

--- commands/modlogs.py
+++ commands/modlogs.py
@@ -11,7 +11,7 @@ class modlogs(commands.Cog):
             await modlogs.set_permissions(modlogs.guild.me, send_messages=True)
         embed=discord.Embed(
             colour=0x8A2BE2,
-            title=f"New Log",
+            title=f"New Audit Log",
             description=f"{moderator.mention} to {user.mention}\n Command: {command}\n Reason: {reason}"
 
         )
