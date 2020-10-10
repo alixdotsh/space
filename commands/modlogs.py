4c4e95ee31d3a26e251f40c2b22ae6488af2a564
Garrison Nichols <58949994+uwu-osu@users.noreply.github.com>
2020-10-10 18:41:38 -0400

    added a feature to select where modlogs goes
    oh and a .txt for my retarded brain to remember things c:

--- commands/modlogs.py
+++ commands/modlogs.py
@@ -1,25 +1,37 @@
 import discord
 from discord.ext import commands
 import asyncpg
+
+
 class modlogs(commands.Cog):
 
     @commands.Cog.listener()
     async def on_modlog(self, user, moderator, command, reason):
-        conn = await asyncpg.connect(user='Space', password='hellowo',
-                                     database='Space', host='127.0.0.1')
+        """Send a message to the modlogs when a moderator does something"""
+
+        # Connect to the database
+        conn = await asyncpg.connect(user='space', password='hellowo', database='space', host='127.0.0.1')
         values = await conn.fetch('''SELECT * FROM modlogs WHERE guild_id = $1''', moderator.guild.id)
         await conn.close()
-        if len (values) == 0:
-            return
-        modlogs = moderator.guild.get_channel (values[0]["modlogs_channel"])
+
+        # See if there were any results, ie if the guild has a modlogs channel
+        if len(values) == 0:
+            modlogs = None
+        else:
+            modlogs = moderator.guild.get_channel (values[0]["modlog_channel"])
+
+        # Create a new modlogs channel if one doesn't exist
         if modlogs is None:
             modlogs = await moderator.guild.create_text_channel(name="mod-logs")
             await modlogs.set_permissions(modlogs.guild.default_role, send_messages=False)
             await modlogs.set_permissions(modlogs.guild.me, send_messages=True)
-            conn = await asyncpg.connect(user='Space', password='hellowo',
-                                         database='Space', host='127.0.0.1')
-            values = await conn.fetch('''INSERT INTO modlogs (guild_id, modlogs_channel) VALUES ($1, $2)''', moderator.guild.id, modlogs.id)
+
+            # Save the ID of the newly created modlogs channel
+            conn = await asyncpg.connect(user='space', password='hellowo', database='space', host='127.0.0.1')
+            await conn.fetch('''INSERT INTO modlogs (guild_id, modlog_channel) VALUES ($1, $2)''', moderator.guild.id, modlogs.id)
             await conn.close()
+
+        # Make and send an embed to the channel
         embed=discord.Embed(
             colour=0x8A2BE2,
             title=f"New Audit Log",
@@ -31,4 +43,3 @@ class modlogs(commands.Cog):
 
 def setup(bot):
     bot.add_cog(modlogs())
-
