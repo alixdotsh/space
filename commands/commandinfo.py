d8df5b9732862cc484fb656736af78f1fd9675ec
Garrison Nichols <58949994+uwu-osu@users.noreply.github.com>
2020-10-11 14:20:56 -0400

    Update commandinfo.py

--- commands/commandinfo.py
+++ commands/commandinfo.py
@@ -4,7 +4,7 @@ class commandinfo(commands.Cog):
 
     #running the command
     @commands.command()
-    async def commandinfo(self, ctx, user: discord.Member):
+    async def commandinfo(self, ctx, user):
 
          # Make and send a message to the channel listing all the possible commands to run
             await ctx.send(f"`-ban`: bans a user\n `-kick`: kicks a user\n `-mute`: mutes a user for x amount of time\n `setchannel_modlogs <channel>`: sets a channel for modlogs to send\n `-accountinfo`: basic account information")
