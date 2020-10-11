73f939a5107e7a8e94d8a2fee90c468106161fef
Garrison Nichols <58949994+uwu-osu@users.noreply.github.com>
2020-10-11 14:18:47 -0400

    fuck embeds at this point

--- commands/commandinfo.py
+++ commands/commandinfo.py
@@ -6,13 +6,8 @@ class commandinfo(commands.Cog):
     @commands.command()
     async def commandinfo(self, ctx, user: discord.Member):
 
-         # Make and send an embed to the channel listing all the possible commands to run
-        embed=discord.Embed(
-            colour=0x8A2BE2,
-            title=f"Executable Commands",
-            description=f"-ban: bans the user\n -kick: kicks the user\n -mute: mutes a user for x amount of time\n -selectchannel_modlogs <channel>: chooses where modlogs go\n -accountinfo: basic account stuff"
-        )
-        await ctx.send(embed=embed)
+         # Make and send a message to the channel listing all the possible commands to run
+            await ctx.send(f"`-ban`: bans a user\n `-kick`: kicks a user\n `-mute`: mutes a user for x amount of time\n `setchannel_modlogs <channel>`: sets a channel for modlogs to send\n `-accountinfo`: basic account information")
 
 def setup(bot):
     bot.add_cog(commandinfo())
