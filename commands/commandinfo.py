c804e5e0d9b8f0cb38ec2a6b37a1b1e8a3c7cfd2
Garrison Nichols <58949994+uwu-osu@users.noreply.github.com>
2020-10-11 23:20:52 -0400

    Update commandinfo.py

--- commands/commandinfo.py
+++ commands/commandinfo.py
@@ -11,12 +11,12 @@ class commandinfo(commands.Cog):
             colour=0x8A2BE2,
             title=f"Executable Commands",
         )
-        embed.add_field(name="-ban", value="bans the user from the server")
-        embed.add_field(name="-kick", value="kicks the user from the server")
-        embed.add_field(name="-mute", value="mutes the user for the given amount of time in minutes")
-        embed.add_field(name="-setchannel_modlogs <channel>", value="selects where modlogs goes")
-        embed.add_field(name="-accountinfo", value="gives you information of the account chosen")
-        embed.add_field(name="-setprefix", value="changes the default prefix")
+        embed.add_field(name="ban", value="bans the user from the server")
+        embed.add_field(name="kick", value="kicks the user from the server")
+        embed.add_field(name="mute", value="mutes the user for the given amount of time in minutes")
+        embed.add_field(name="setchannel_modlogs <channel>", value="selects where modlogs goes")
+        embed.add_field(name="accountinfo", value="gives you information of the account chosen")
+        embed.add_field(name="setprefix", value="changes the default prefix")
         await ctx.send(embed=embed)
 
 def setup(bot):
